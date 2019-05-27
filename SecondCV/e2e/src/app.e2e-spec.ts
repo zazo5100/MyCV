92f4c7e34b28dccb1133f0e6e55a552c330d83f3
AAziz K Bawajeeh <zazo5100@gmail.com>
2019-05-27 13:05:05 +0300

    Deletr First CV

--- SecondCV/e2e/src/app.e2e-spec.ts
+++ SecondCV/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to FirstCV!');
+    expect(page.getTitleText()).toEqual('Welcome to SecondCV!');
   });
 
   afterEach(async () => {
